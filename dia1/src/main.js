5f428b024079a500d943d971f7a288e5176c8858
Jonas Teixeira <jonaslagoteixeira@gmail.com>
2021-08-17 15:00:11 -0300

    Dia 1 - Exercicio 4 - adicionado link e evento de click

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,20 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const link = document.querySelector('[data-js="link"]')
+const linkLabel = document.querySelector('[data-js="label"]')
+const app = document.querySelector('[data-js="app"]')
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+link.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const ariaValue = app.getAttribute('aria-hidden') === 'false' ? 'true' : 'false'
+
+  linkLabel.innerHTML = ariaValue === 'true' ? 'Exibir' : 'Desaparecer'
+
+  app.setAttribute('aria-hidden', ariaValue)
+})
