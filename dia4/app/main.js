39cb070a936389db6ddf9b0eccea4f82f463ed06
Jonas Teixeira <jonaslagoteixeira@gmail.com>
2021-08-21 12:47:30 -0300

    Dia 4 - Exercicio 3 - feito cadastro de carros

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,7 +1,10 @@
 import './style.css'
 
 const url = 'http://localhost:3333/cars'
+const toast = document.querySelector('[data-js="toast"]')
+const form = document.querySelector('[data-js="form-car"]')
 const tbody = document.querySelector('[data-js="table-body"]')
+const inputImage = document.querySelector('[data-js="input-image"]')
 
 const addProperties = {
   image: (rowElement, dataElement, value) => {
@@ -31,6 +34,7 @@ const addProperties = {
 const setNoCarsMessage = () => {
   const tr = document.createElement('tr')
   const td = document.createElement('td')
+  tr.dataset.js = 'no-cars'
   td.colSpan = 5
   td.className = 'td'
   td.textContent = 'Nenhum carro encontrado'
@@ -39,7 +43,7 @@ const setNoCarsMessage = () => {
   tbody.appendChild(tr)
 }
 
-const addCars = (cars) => {
+const listCars = (cars) => {
   cars.forEach((car) => {
     const tr = document.createElement('tr')
 
@@ -62,10 +66,68 @@ const getCars = async () => {
 
     const cars = await response.json()
 
-    cars.length ? addCars(cars) : setNoCarsMessage()
+    cars.length ? listCars(cars) : setNoCarsMessage()
   } catch (error) {
-    console.warn('error:', error)
+    addToast('Ocorreu um erro, tente mais tarde')
+    setNoCarsMessage()
   }
 }
+
+const addToast = (message) => {
+  toast.textContent = message
+  toast.classList.add('show')
+
+  setTimeout(() => {
+    toast.classList.remove('show')
+  }, 2500)
+}
+
+const addCars = async (car) => {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(car)
+    })
+
+    const data = await response.json()
+
+    if(data.error) {
+      throw data.message
+    }
+
+    addToast(data.message)
+    listCars([{...car}])
+
+    form.reset()
+    inputImage.focus()
+  } catch (error) {
+    addToast(error)
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const formElements = [...e.target.elements].slice(0, -1)
+
+  const car = {
+    image: formElements[0].value,
+    brandModel: formElements[1].value,
+    year: formElements[2].value,
+    plate: formElements[3].value,
+    color: formElements[4].value
+  }
+
+  const noCarsMessage = document.querySelector('[data-js="no-cars"]')
+
+  if(noCarsMessage) {
+    noCarsMessage.remove()
+  }
+
+  addCars(car)
+})
 
 getCars()
